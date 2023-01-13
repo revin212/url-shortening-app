c7be77232b9506468a59b12bd1fc0d795b244ca0
Eileen D'Angelo <eileenrkerekes@gmail.com>
2023-01-13 07:08:16 -0500

    refactor:ResultList.js  -add ref to copy link

--- src/ResultList.js
+++ src/ResultList.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useCallback } from "react";
 
 const ResultList = (props) => {
     let results = props.results;//get the result array from the props
@@ -6,10 +6,19 @@ const ResultList = (props) => {
         results = props.results;
     }, [props])
 
+// useRef //
+    const ref = useCallback(node => {
+        if (node !== null) {
+            // do something with the node
+            navigator.clipboard.writeText(node.innerText);
+        }
+    }, []);
+
+
     function copyLink(id){      //copy the shortened url to clipboard
-        const shortedLink = document.getElementById(`result-${id}`)
+        // const shortedLink = document.getElementById(`result-${id}`)
         const copyBtn = document.getElementById(`btn-${id}`)
-        navigator.clipboard.writeText(shortedLink.innerText);   //copy the shortened url to clipboard
+        // navigator.clipboard.writeText(shortedLink.innerText);   //copy the shortened url to clipboard
         copyBtn.innerText = 'Copied!'
         if(copyBtn.classList.contains('bg-primary-cyan')){
             copyBtn.classList.remove('bg-primary-cyan')
@@ -30,7 +39,7 @@ const ResultList = (props) => {
                     <p className="link mb-5 lg:mb-0 lg:w-[650px] break-words">{ result.oriLink }</p>
                     <div className="flex justify-end flex-col lg:flex-row lg:items-center">
                         {/* shorten link: */}
-                        <p id={`result-${result.id}`} className="shorten-link mb-3 text-primary-cyan lg:mb-0 ">{ result.shortLink }</p>
+                        <p ref={ref} id={`result-${result.id}`} className="shorten-link mb-3 text-primary-cyan lg:mb-0 ">{ result.shortLink }</p>
                         <button id={`btn-${result.id}`} onClick={()=>copyLink(result.id)} className="font-bold w-full lg:w-[130px] bg-primary-cyan text-white cursor-pointer hover:bg-primary-cyan-hover py-3 px-6 rounded-lg lg:ml-6">Copy</button>
                     </div>
                 </div>
